e6411ef998587aa1938d1a15e5b41d52075d6f4b
сергей токарев <sseregatokarew@gmail.com>
2022-02-18 19:00:07 +0700

    Добавьте файлы проекта.

---
+++ REC-95/REC-95.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace REC_95
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату Х вершины А ");
+            var x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату Y вершины А ");
+            var y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату Х вершины B ");
+            var x2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату Y вершины B ");
+            var y2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату Х вершины C ");
+            var x3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату Y вершины C ");
+            var y3 = int.Parse(Console.ReadLine());
+            Point a = new Point(x1, y1);
+            Point b = new Point(x2, y2);
+            Point c = new Point(x3, y3);
+            Triangle Triangle0 = new Triangle(a, b, c);
+            Triangle0.showPoints();
+            Triangle0.showSides();
+            Triangle0.showPerimetr();
+            Triangle0.showArea();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ REC-95/Triangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REC_95
+{
+    class Triangle
+    {
+        public Point A { set; get; }
+        public Point B { set; get; }
+        public Point C { set; get; }
+
+        public Double AB;
+        public Double BC;
+        public Double CA;
+        public Double P;
+        public Double S;
+
+        public Triangle(Point a, Point b, Point c)
+        {
+            A = a;
+            B = b;
+            C = c;
+            AB = Math.Sqrt(Math.Pow((B.X - A.X), 2) + Math.Pow((B.Y - A.Y), 2));
+            BC = Math.Sqrt(Math.Pow((C.X - B.X), 2) + Math.Pow((C.Y - B.Y), 2));
+            CA = Math.Sqrt(Math.Pow((A.X - C.X), 2) + Math.Pow((A.Y - C.Y), 2));
+            P = (AB + BC + CA);
+            S = Math.Sqrt(P / 2 * (P / 2 - AB) * (P / 2 - BC) * (P / 2 - CA));/* формула герона*/
+
+            if ((AB >= BC + CA) | (BC >= AB + CA) | (CA >= AB + BC))
+            {
+                throw new ArgumentNullException(nameof(Triangle));
+            }
+        }
+        public void showPoints()
+        {
+            Console.WriteLine($"\nA({A.X},{A.Y})\nB({B.X},{B.Y})\nC({C.X},{C.Y})\n");
+        }
+        public void showSides()
+        {
+            Console.WriteLine($"AB = {AB}\nBC = {BC}\nCA = {CA}\n");
+        }
+        public void showPerimetr()
+        {
+            Console.WriteLine($"P = {P}\n");
+        }
+        public void showArea()
+        {
+            Console.WriteLine($"S = {S}");
+        }
+    }
+
+}
